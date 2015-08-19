be57c0d644cd105c5500b3be17bab7b1b35c15cc
Anton Vlasenko <anton.vlasenko@tictail.com>
2015-08-19 10:48:40 +0200

    Using react-intl for time rendering with i18n support

--- timeago.js
+++ timeago.js
@@ -1,127 +1,85 @@
-'use strict'
+'use strict';
 
-var React = require('react')
-var assign = require('react/lib/Object.assign')
+var React = require('react');
 
-module.exports = React.createClass(
-  { displayName: 'Time-Ago'
-  , timeoutId: 0
-  , getDefaultProps: function(){
-      return { live: true
-             , component: 'span'
-             , minPeriod: 0
-             , maxPeriod: Infinity
-             , formatter: function (value, unit, suffix) {
-                 if(value !== 1){
-                   unit += 's'
-                 }
-                 return value + ' ' + unit + ' ' + suffix
-               }
-             }
-    }
-  , propTypes:
-      { live: React.PropTypes.bool.isRequired
-      , minPeriod: React.PropTypes.number.isRequired
-      , maxPeriod: React.PropTypes.number.isRequired
-      , component: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.func]).isRequired
-      , formatter: React.PropTypes.func.isRequired
-      , date: React.PropTypes.oneOfType(
-          [ React.PropTypes.string
-          , React.PropTypes.number
-          , React.PropTypes.instanceOf(Date)
-          ]
-        ).isRequired
+var IntlMixin = require('react-intl').IntlMixin;
+var FormattedRelative = require('react-intl').FormattedRelative;
+
+module.exports = React.createClass({
+    mixins: [IntlMixin],
+    displayName: 'Time-Ago',
+    timeoutId: 0,
+    getDefaultProps: function () {
+      return {
+        live: true,
+        minPeriod: 0,
+        maxPeriod: Infinity
+      };
+    },
+    propTypes: {
+      live: React.PropTypes.bool.isRequired,
+      minPeriod: React.PropTypes.number.isRequired,
+      maxPeriod: React.PropTypes.number.isRequired,
+
+      format: React.PropTypes.string,
+      value: React.PropTypes.any.isRequired,
+      now: React.PropTypes.any
+    },
+    componentDidMount: function () {
+      if (this.props.live) {
+        this.tick(true);
       }
-  , componentDidMount: function(){
-      if(this.props.live) {
-        this.tick(true)
-      }
-    }
-  , componentDidUpdate: function(lastProps){
-      if(this.props.live !== lastProps.live || this.props.date !== lastProps.date){
-        if(!this.props.live && this.timeoutId){
+    },
+    componentDidUpdate: function (lastProps) {
+      if (this.props.live !== lastProps.live || this.props.date !== lastProps.date) {
+        if (!this.props.live && this.timeoutId) {
           clearTimeout(this.timeoutId);
           this.timeoutId = undefined;
         }
-        this.tick()
+        this.tick();
       }
-    }
-  , componentWillUnmount: function() {
-    if(this.timeoutId) {
-      clearTimeout(this.timeoutId);
-      this.timeoutId = undefined;
+    },
+    componentWillUnmount: function () {
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+        this.timeoutId = undefined;
+      }
+    },
+    tick: function (refresh) {
+      if (!this.isMounted() || !this.props.live) {
+        return;
+      }
+
+      var period = 1000;
+
+      var then = (new Date(this.props.value)).valueOf();
+      var now = this.props.now ? (new Date(this.props.now)).valueOf() : Date.now();
+      var seconds = Math.round(Math.abs(now - then) / 1000);
+
+      if (seconds < 45) {
+        period = 1000;
+      } else if (seconds < 60) {
+        period = 1000 * 15;
+      } else if (seconds < 60 * 60) {
+        period = 1000 * 60;
+      } else if (seconds < 60 * 60 * 24) {
+        period = 1000 * 60 * 60;
+      } else {
+        period = 0;
+      }
+
+      period = Math.min(Math.max(period, this.props.minPeriod), this.props.maxPeriod);
+
+      if (period) {
+        this.timeoutId = setTimeout(this.tick, period);
+      }
+
+      if (!refresh) {
+        this.forceUpdate();
+      }
+    },
+    render: function () {
+      return React.createElement(FormattedRelative, this.props);
     }
   }
-  , tick: function(refresh){
-      if(!this.isMounted() || !this.props.live){
-        return
-      }
-
-      var period = 1000
-
-      var then = (new Date(this.props.date)).valueOf()
-      var now = Date.now()
-      var seconds = Math.round(Math.abs(now-then)/1000)
-
-      if(seconds < 60){
-        period = 1000
-      } else if(seconds < 60*60) {
-        period = 1000 * 60
-      } else if(seconds < 60*60*24) {
-        period = 1000 * 60 * 60
-      } else {
-        period = 0
-      }
-
-      period = Math.min(Math.max(period, this.props.minPeriod), this.props.maxPeriod)
-
-      if(!!period){
-        this.timeoutId = setTimeout(this.tick, period)
-      }
-
-      if(!refresh){
-        this.forceUpdate()
-      }
-    }
-  , render: function(){
-      var then = (new Date(this.props.date)).valueOf()
-      var now = Date.now()
-      var seconds = Math.round(Math.abs(now-then)/1000)
-
-      var suffix = then < now ? 'ago' : 'from now'
-
-      var value, unit
-
-      if(seconds < 60){
-        value = Math.round(seconds)
-        unit = 'second'
-      } else if(seconds < 60*60) {
-        value = Math.round(seconds/60)
-        unit = 'minute'
-      } else if(seconds < 60*60*24) {
-        value = Math.round(seconds/(60*60))
-        unit = 'hour'
-      } else if(seconds < 60*60*24*7) {
-        value = Math.round(seconds/(60*60*24))
-        unit = 'day'
-      } else if(seconds < 60*60*24*30) {
-        value = Math.round(seconds/(60*60*24*7))
-        unit = 'week'
-      } else if(seconds < 60*60*24*365) {
-        value = Math.round(seconds/(60*60*24*30))
-        unit = 'month'
-      } else {
-        value = Math.round(seconds/(60*60*24*365))
-        unit = 'year'
-      }
-
-      var props = assign({}, this.props)
-
-      delete props.date
-      delete props.formatter
-      delete props.component
-
-      return React.createElement( this.props.component, props, this.props.formatter(value, unit, suffix, then) )
-    }
-  }
-)
+);
