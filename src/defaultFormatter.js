2b095708715273cfc1ab5fea5977dcaa2478caa2
Johnny Loi <theaznshumai@gmail.com>
2017-03-16 09:46:25 -0700

    fix default formatter
    Why:
    
    * Default formatter was output pluralized units when it should be singluar

--- src/defaultFormatter.js
+++ src/defaultFormatter.js
@@ -1,7 +1,7 @@
 // @flow
 
 export default function defaultFormatter (value: number, unit: string, suffix: string): string {
-  if ((value % 10 !== 1) || (value % 100 === 11)) {
+  if (value !== 1) {
     unit += 's'
   }
   return value + ' ' + unit + ' ' + suffix
