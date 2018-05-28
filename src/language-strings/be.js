74f09a1fbf3150586754ef8425015c01a22ea0e1
Andrew Avdeev <andrewww.avdeev@gmail.com>
2018-05-28 22:21:31 +0300

    Changed comment

--- src/language-strings/be.js
+++ src/language-strings/be.js
@@ -1,7 +1,7 @@
 /* @flow */
 import type { L10nsStrings } from '../formatters/buildFormatter'
 
-// Russian
+// Belarusian
 function numpf(n: number, f: string, s: string, t: string): string {
   // f - 1, 21, 31, ...
   // s - 2-4, 22-24, 32-34 ...
