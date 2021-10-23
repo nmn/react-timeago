8e6df6838814f1291d05b2926cad2d7494247feb
XHunter <kushwah.kartikey14@stu.upes.ac.in>
2021-10-23 15:29:22 +0530

    Added Hindi language

---
+++ src/language-strings/hi.js
@@ -0,0 +1,24 @@
+/* @flow */
+import type { L10nsStrings } from '../formatters/buildFormatter'
+
+// Hindi (Template)
+const strings: L10nsStrings = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: 'पहले',
+  suffixFromNow: 'अभी से',
+  seconds: 'एक मिनट से कम',
+  minute: 'मिनट',
+  minutes: '%d मिनट्स',
+  hour: 'करीब एक घंटा',
+  hours: 'करीब %d घंटा',
+  day: 'दिन',
+  days: '%d दिन',
+  month: 'तक़रीबन एक महीना',
+  months: '%d महीना',
+  year: 'लगभग एक साल',
+  years: '%d साल',
+  wordSeparator: ' ',
+}
+
+export default strings
