dc75408fd3f4037cde4623a8334ff531b9a3fc1e
Justin Shaw <realjustinshaw@gmail.com>
2022-06-16 11:07:16 -0700

    support for fuzzy intervals

---
+++ src/language-strings/en-fuzzy.js
@@ -0,0 +1,24 @@
+/* @flow */
+import type { L10nsStrings } from '../formatters/buildFormatter'
+
+// English (Template)
+const strings: L10nsStrings = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: 'ago',
+  suffixFromNow: 'from now',
+  seconds: 'a moment',
+  minute: 'about a minute',
+  minutes: '%d minutes',
+  hour: 'about an hour',
+  hours: 'about %d hours',
+  day: 'a day',
+  days: '%d days',
+  month: 'about a month',
+  months: '%d months',
+  year: 'about a year',
+  years: '%d years',
+  wordSeparator: ' ',
+}
+
+export default strings
