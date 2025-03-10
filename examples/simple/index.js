27b27570fb169cbcd49e9f1c1335263d31d2fb9a
Naman Goel <naman34@gmail.com>
2025-03-09 21:18:18 -0700

    v8.0.0

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,14 +1,13 @@
-/* @flow */
+/* @flow strict */
 import * as React from 'react'
-import ReactDom from 'react-dom'
+import ReactDom from 'react-dom/client'
 import TimeAgo from '../../src/index'
 
 const appElement = document.getElementById('app')
 
-appElement &&
-  ReactDom.render(
-    <div>
-      You opened this page <TimeAgo date={Date.now()} />
-    </div>,
-    appElement,
-  )
+appElement && ReactDom.createRoot(appElement).render(
+  <div>
+    You opened this page <TimeAgo date={Date.now()} />
+  </div>,
+)
+
