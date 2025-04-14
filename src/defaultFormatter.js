5e8bc5c82c0778800d92218c52e2fc7392f058ed
Naman Goel <nmn@users.noreply.github.com>
2025-04-13 17:49:58 -0700

    feat: Add new intl-based formatter builder (#227)
    * feat: Add new intl-based formatter builder
    
    * fixes
    
    * add updated example
    
    * fix type error
    
    * remove duplicate object key

--- src/defaultFormatter.js
+++ src/defaultFormatter.js
@@ -1,11 +1,10 @@
 // @flow
 
-
-import type { Formatter } from "./types";
+import type { Formatter, Suffix, Unit } from './types'
 
 const defaultFormatter: Formatter = (
   value: number,
-  _unit: string,
+  _unit: Unit,
   suffix: string,
 ): string => {
   const unit = value !== 1 ? _unit + 's' : _unit
@@ -13,3 +12,28 @@ const defaultFormatter: Formatter = (
 }
 
 export default defaultFormatter
+
+export type IntlFormatterOptions = $ReadOnly<{
+  locale?: void | string,
+  localeMatcher?: 'lookup' | 'best fit',
+  numberingSystem?: Intl$NumberingSystem,
+  style?: 'long' | 'short' | 'narrow',
+  numeric?: 'always' | 'auto',
+}>
+
+export const makeIntlFormatter: (IntlFormatterOptions) => Formatter =
+  ({ locale, ...options }) =>
+  (value, unit, suffix) => {
+    const RelativeTimeFormat = Intl.RelativeTimeFormat
+    if (!RelativeTimeFormat) {
+      throw new Error('Intl.RelativeTimeFormat is not supported')
+    }
+
+    const rtf = new RelativeTimeFormat(locale ?? undefined, {
+      style: 'long',
+      numeric: 'auto',
+      ...options,
+    })
+
+    return rtf.format(suffix === 'ago' ? -value : value, unit)
+  }
